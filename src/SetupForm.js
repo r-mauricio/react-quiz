3c4765bdefcf4f4821c895b8ce643ac17357754b
Rafael Mauricio <rafaels.m@outlook.com>
2021-11-21 11:28:41 -0300

    First Commit

---
+++ src/SetupForm.js
@@ -0,0 +1,73 @@
+import React from "react";
+import { useGlobalContext } from "./Context";
+
+const SetupForm = () => {
+  const { quiz, handleChange, handleSubmit, error } = useGlobalContext();
+  return (
+    <main>
+      <section className='quiz quiz-small'>
+        <form className='setup-form'>
+          <h2>awesome quiz</h2>
+          {/* amount */}
+          <div className='form-control'>
+            <label htmlFor='amount'>number of questions</label>
+            <input
+              type='number'
+              name='amount'
+              id='amount'
+              value={quiz.amount}
+              onChange={handleChange}
+              className='form-input'
+              min={1}
+              max={50}
+            />
+          </div>
+          {/* category */}
+
+          <div className='form-control'>
+            <label htmlFor='category'>category</label>
+            <select
+              name='category'
+              id='category'
+              className='form-input'
+              value={quiz.category}
+              onChange={handleChange}
+            >
+              <option value='television'>Television</option>
+              <option value='videogames'>Videogames</option>
+              <option value='Science: Computers'>Science: Computers</option>
+              <option value='sports'>Sports</option>
+              <option value='history'>History</option>
+            </select>
+          </div>
+          {/* difficulty */}
+
+          <div className='form-control'>
+            <label htmlFor='difficulty'>Select Difficulty</label>
+            <select
+              name='difficulty'
+              id='difficulty'
+              className='form-input'
+              value={quiz.difficulty}
+              onChange={handleChange}
+            >
+              <option value='easy'>Easy</option>
+              <option value='medium'>Medium</option>
+              <option value='hard'>Hard</option>
+            </select>
+          </div>
+          {error && (
+            <p className='error'>
+              can't generate questions, please try different options
+            </p>
+          )}
+          <button type='submit' onClick={handleSubmit} className='submit-btn'>
+            start
+          </button>
+        </form>
+      </section>
+    </main>
+  );
+};
+
+export default SetupForm;
